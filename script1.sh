78fac6e107b084427faaa2be1974315725e7b4de
SachinUmbare <sachinumbare31@gmail.com>
2024-06-06 16:00:22 +0530

    This is my First Practical

--- script1.sh
+++ script1.sh
@@ -4,7 +4,7 @@ echo "Happy weekend"
 echo "Hello batch-24, We are learning git"
 echo "Please complete your practicals on daily basis"
 echo "hello friends"
-echo "hello friends I am Learning Devops"
+echo "This is My Frist Practical"
 
 
 
