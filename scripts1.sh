3ec076786a1fa4a3c6619c5040f30f8fa5692f54
root <root@ip-172-31-25-236.ec2.internal>
2024-06-07 05:14:50 +0000

    This is my Practal

--- scripts1.sh
+++ scripts1.sh
@@ -1,4 +1,4 @@
-echo"we are trying this practical from linux EC2"
 echo "we are from batch-24 and learning git/github"
-echo " happy weekend"
-echo " we learning git"
+echo " I am Learing AWS and Devops at Young Minds Institute"
+echo " My Name is Sachin Umbare"
+echo " I am From Indapur Dist Pune"
